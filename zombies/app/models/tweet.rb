d7092a4fac1e95886dce0b599f4fe93cbb270b0a
Danny Wu <wushuo1988@hotmail.com>
2014-04-02 22:18:51 -0400

    created tweets

---
+++ zombies/app/models/tweet.rb
@@ -0,0 +1,4 @@
+class Tweet < ActiveRecord::Base
+	validates :status, presence: true,
+	belongs_to :zombie
+end
--- zombies/config/routes.rb
+++ zombies/config/routes.rb
@@ -6,7 +6,7 @@ Zombies::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # get 'tweet#index', as: 'store'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
